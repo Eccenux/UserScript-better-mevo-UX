52aa80e20d52aebe711eca50d9a5a7cedad1f33a
Eccenux <eccenux+git@gmail.com>
2019-03-26 22:07:47 +0100

    0.0.5 mark

--- better-mevo-UX.meta.js
+++ better-mevo-UX.meta.js
@@ -1,8 +1,8 @@
 ﻿// ==UserScript==
 // @name         Rower Mevo UX
 // @namespace    pl.enux.rowermevo
-// @version      0.0.1
-// @description  [0.0.1] Poprawki UX dla witryny Roweru Mevo.
+// @version      0.0.5
+// @description  [0.0.5] Poprawki UX dla witryny Roweru Mevo.
 // @author       Eccenux
 // @match        https://rowermevo.pl/*
 // @grant        GM_addStyle
